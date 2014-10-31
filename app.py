32d0b1170a4f73fbc69295b8222045488dd0b662
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2014-10-31 12:30:15 -0400

    bug fixes

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 @app.route('/login',methods=['GET','POST'])
 def login():
     if request.method=='GET':
-        render_template('login.html')
+        return render_template('login.html')
     else:
         user=request.form['user']
         pw=request.form['pass']
@@ -14,7 +14,7 @@ def login():
 @app.route('/register',methods=['GET','POST'])
 def register():
     if request.method=='GET':
-        render_template('register.html')
+        return render_template('register.html')
     else:
         name=request.form['name']
         user=request.form['user']
@@ -29,19 +29,19 @@ def logout():
 
 @app.route('/about')
 def about():
-    render_template('about.html')
+    return render_template('about.html')
 
 @app.route('/home')
 def home():
     ##check if logged in
-    render_template('home.html')
+    return render_template('home.html')
     ##else, redirect to login and store home with sessions
     
 @app.route('/setting',methods=['GET','POST'])
 def settings():
     ##check if logged in
     if request.method=='GET':
-        render_template('settings.html')
+        return render_template('settings.html')
     else:
         ##get new info and update
         pass
