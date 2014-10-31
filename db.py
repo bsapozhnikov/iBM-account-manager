4fd23462e8752d48f17eb63afd5a4a52090f6bd8
Michael Huang <Mikester591@gmail.com>
2014-10-31 12:26:19 -0400

    Added Py file

--- db.py
+++ db.py
@@ -9,7 +9,7 @@ print db.collection_names()
 
 def registerUser(user,name,color,pw):
     x = db.users.find_one({"user":user})
-    if x == None && pw != None:
+    if x == None and pw != None:
         db.users.insert({"user":user,
                          "name":name,
                          "color":color,
@@ -20,7 +20,23 @@ def registerUser(user,name,color,pw):
 
 def updateUserInfo(user,pw,newpw,name,color):
     x = db.users.find_one({"user":user})
-    if x[password] == pw:
+    if x["password"] == pw:
+        if name == None:
+            name =x["name"]
+        if newpw == None:
+            newpw = x["password"]
+        if color == None:
+            color = x["color"]
+        db.users.update({"user":user},{"password":newpw,
+                                       "name":name,
+                                       "color":color})
+        return True
+    else:
+        return False
+
+
+            
+                                    
         
     
 
